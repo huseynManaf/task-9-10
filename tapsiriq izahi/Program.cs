4d54a57c690781afa9ee8dbc53dd8169d0024757
huseynManaf <huseynim@code.edu.az>
2023-01-05 07:19:02 -0800

    init

---
+++ tapsiriq izahi/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace tapsiriq_izahi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task9
+            //int n =int.Parse(Console.ReadLine());
+            //int[] mass = new int[n];
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    Console.Write($"mass[{i}]");
+            //mass[i] = int.Parse(Console.ReadLine()); 
+
+            //}
+            //int max = 0;
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    if (mass[max] < mass[i])
+            //        max = i;
+            //}
+            //Console.WriteLine($"max indeks = [{max}]");
+            #endregion
+            {
+                #region task 10
+                int n = int.Parse(Console.ReadLine());
+                int[] mass = new int[n];
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    Console.Write($"mass[{i}]");
+                    mass[i] = int.Parse(Console.ReadLine());
+
+                }
+                int min = 0;
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    if (mass[min] > mass[i])
+                        min = i;
+                }
+                Console.WriteLine($"max indeks = [{min}]");
+                #endregion
+            }
+        }
+    }
+}
